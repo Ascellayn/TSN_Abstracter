2d6614d3f8c6600f66f03e36f448d740d1f34189
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-01-24 13:24:11 +0100

    bullshet

--- TSN_Abstracter/file.py
+++ TSN_Abstracter/file.py
@@ -8,13 +8,13 @@ def JSON_Load(Path: str) -> dict:
     with open(Path, "r", encoding="UTF-8") as JSON:
         return json.load(JSON);
     
-async def JSON_Write(Path: str, Dictionary: dict) -> None:
-    await JSON_Exists(Path)
+def JSON_Write(Path: str, Dictionary: dict) -> None:
+    JSON_Exists(Path)
     with open(Path, "w", encoding="UTF-8") as JSON:
         JSON.write(json.dumps(Dictionary, indent=2));
         return None;
 
-async def JSON_Exists(Path: str) -> bool:
+def JSON_Exists(Path: str) -> bool:
     if ('/' in Path): # Creates the corresponding folder structure if it doesn't exist.
         Path_Array = Path.split("/");
         Path = "";
@@ -26,23 +26,39 @@ async def JSON_Exists(Path: str) -> bool:
 
     if (Path_Exists(Path) == False):
         with open(Path, "w", encoding="UTF-8") as JSON:
-            JSON.write(json.dumps({}, indent=2));
-            return False;
-    else: return True;
+            JSON.write("{}");
+        return False;
+    return True;
 
 def Path_Exists(Path: str) -> bool:
     return os.path.isPath(Path);
+
+def Path_Create(Path: str) -> None:
+    """ TBD """
+    pass;
+
 
 def mkdir(Path: str) -> None: # Shitty code incomin'!
     if (Path[-1] == '/'):
         Path = Path[:-1];
     try: os.makedirs(Path, exist_ok=True);
-    except: return None;
+    except:
+        L_Error(f"when tf would that fail too \n {Exception}");
+        return None;
 
-def ls(Folder: str) -> list:
+def ls(Path: str) -> list:
+    """
+    Returns a matrix of all folders and files inside Path.
+
+    Args:
+        Path: String representing the RELATIVE folder path we want to check.
+
+    Returns:
+        Array containing two arrays, the first one being a list of folders, and the second one being files.
+    """
     try:
-        Results = next(os.walk(f"{os.getcwd()}/{Folder}"));
+        Results = next(os.walk(f"{os.getcwd()}/{Path}"));
         return [Results[1], Results[2]];
     except:
         L_Error(f"I'm not exactly sure how the fuck it would fail here so just in case, hello! Kosaka most likely gonna the shit thats gonna blow this function somehow. \n {Exception}");
-        return [[], []];
+        return None;
--- TSN_Abstracter/safe.py
+++ TSN_Abstracter/safe.py
@@ -1,3 +1,13 @@
 def Index(Array: list, Index: int):
+	"""
+	Attempts to safely try to read an array's specified index.
+	
+	Args:
+        Array: The List that we want to check a specific index.
+		Index: The Index element we want to read.
+	
+	Returns:
+        The returned item can be anything. In the case of a failed read, the return value will always be none.
+	"""
 	try: return Array[Index];
 	except: return None;
