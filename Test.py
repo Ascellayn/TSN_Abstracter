d2a0077087c6e9fe149cb0029ed81158481b6646
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-02-13 15:20:44 +0100

    Retarded bad code ("Await" Status Updates, proper tabbing for exceptions)

--- Test.py
+++ Test.py
@@ -2,18 +2,24 @@ from TSN_Abstracter import *;
 import time;
 
 Log.Delete();
-Config.Logging["File"] = True;
-Log.Critical("Im the only log to be written bitches");
-Config.Logging["File"] = False;
 
 Log.Info(File.Tree("TSN_Abstracter"));
 Log.Info(File.Tree("IDontFuckingExist"));
 
+Config.Logging["File"] = True;
+Log.Critical("Im the only log to be written bitches, for now");
+
 File.JSON_Write("JSON/Test.json", {"hi": "balls"});
 
+Log.Info("Hey, I'm waiting for status shit here...");
+Log.Critical("im gonna fucc shi up now")
+Log.OK();
+
 Log.Warning("Doing LITERALLY nothing right now...");
-time.sleep(2);
+time.sleep(1);
 Log.Status_Update("[DONE]");
+
+Config.Logging["File"] = False;
 
 """
 Unix = 0;
