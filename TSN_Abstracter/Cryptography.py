b6c74da5112b2272376c3b41f23f97ea34704023
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-02-21 17:46:16 +0100

    Cryptography + Use of | instead of typing.Union

---
+++ TSN_Abstracter/Cryptography.py
@@ -0,0 +1,55 @@
+import hashlib
+
+import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519;
+import cryptography.hazmat.primitives.asymmetric.rsa as rsa;
+import cryptography.hazmat.primitives.asymmetric.padding as padding;
+import cryptography.hazmat.primitives.hashes as hashes;
+
+def Generate_Key() -> rsa.RSAPrivateKey | rsa.RSAPublicKey:
+    """ Generates a pair of RSA Private/Public Keys with a size of 8912 bits. """
+    Private_Key = rsa.generate_private_key(
+        public_exponent = 65537,
+        key_size = 8192
+    );
+    Public_Key = Private_Key.public_key();
+    return Private_Key, Public_Key;
+
+def Encrypt(Public_Key: rsa.RSAPublicKey, Data: str) -> bytes:
+    """ Encrypts Data using RSA-8192 and returns Bytes. """
+    return Public_Key.encrypt(
+        Data.encode("utf-8"),
+        padding.OAEP(
+            padding.MGF1(hashes.SHA3_512()),
+            hashes.SHA3_512(),
+            None
+        )
+    );
+
+def Decrypt(Private_Key: rsa.RSAPrivateKey, Data: str) -> str:
+    """ Decrypts Data using RSA-8192 and returns as a String. """
+    return Private_Key.decrypt(
+        Data.encode("utf-8"),
+        padding.OAEP(
+            padding.MGF1(hashes.SHA3_512()),
+            hashes.SHA3_512(),
+            None
+        )
+    ).decode("utf-8");
+
+def Hash(Data: str) -> str:
+    """ Returns the SHA3_512 Hex Hash of Data """
+    return (hashlib.sha3_512(Data.encode("utf-8"))).hexdigest();
+
+
+# To not be used, I couldn't read and coded these in not realizing I wanted RSA instead.
+def Generate_Signature() -> ed25519.Ed25519PrivateKey | ed25519.Ed25519PublicKey:
+    Private_Key = ed25519.Ed25519PrivateKey.generate();
+    Public_Key = Private_Key.public_key();
+    return Private_Key, Public_Key;
+
+def Verify_Signature(Public_Key: ed25519.Ed25519PublicKey, Signature: bytes, Data: str) -> bool:
+    try:
+        Public_Key.verify(Signature, Data.encode("utf-8"));
+        return True;
+    except:
+        return False;
