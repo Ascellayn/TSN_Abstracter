a3e2451f522c615552c888dba3235f49fa2307f0
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-03-16 13:00:14 +0100

    Forgotten push (STL)

--- TSN_Abstracter/Misc.py
+++ TSN_Abstracter/Misc.py
@@ -1,3 +1,15 @@
+import threading, multiprocessing;
+
 def Void() -> None: 
     """ Does absolutely nothing. """
     return None;
+
+def Thread_Start(Function, Daemon: bool = True) -> None:
+    """ Abstraction to launch a thread, takes in a function and a boolean if the thread should be a daemon or not. """
+    threading.Thread(target=Function()).setDaemon(Daemon).start();
+    return;
+
+def Process_Start(Function, Daemon: bool = True) -> None:
+    """ Abstraction to launch a Process. """
+    multiprocessing.Process(target=Function()).start();
+    return;
