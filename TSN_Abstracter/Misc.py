7eeba90132e2f3accc861dc76276d52bc4c2b537
Ascellayn <root.siriusb@gmail.com>
2025-03-29 20:19:49 +0100

    Fix Thread_Start being single handedly the worst shid i ever wrote

--- TSN_Abstracter/Misc.py
+++ TSN_Abstracter/Misc.py
@@ -4,9 +4,13 @@ def Void() -> None:
     """ Does absolutely nothing. """
     return None;
 
-def Thread_Start(Function, Daemon: bool = True) -> None:
+def Thread_Start(Function, Arguments: tuple = (), Daemon: bool = True) -> None:
     """ Abstraction to launch a thread, takes in a function and a boolean if the thread should be a daemon or not. """
-    threading.Thread(target=Function()).setDaemon(Daemon).start();
+    threading.Thread(
+        target=Function,
+        args=Arguments, 
+        daemon=Daemon
+    ).start();
     return;
 
 def Process_Start(Function, Daemon: bool = True) -> None:
