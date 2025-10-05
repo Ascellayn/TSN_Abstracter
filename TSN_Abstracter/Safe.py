4b3fc410203b272de8732ef448a55755060d86e2
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2025-10-05 02:01:20 +0200

    Safe | DocString Overhaul

--- TSN_Abstracter/Safe.py
+++ TSN_Abstracter/Safe.py
@@ -1,12 +1,32 @@
-def Index(Array: list, Index: int): # Isn't there a Python function to already do this?
+"""
+This module from TSN Abstracter is in charge of providing ways to access elements without causing exceptions.
+
+## Examples
+>>> from TSN_Abstracter import Safe;
+>>> Array: list[str] = ["Arellayn was here"];
+>>> Safe.Index(Array, 0);
+"Arellayn was here"
+>>> Safe.Index(Array, 1);
+None
+"""
+
+def Index(Array: list[object], Index: int) -> object: # Isn't there a Python function to already do this?
 	"""
 	Attempts to safely try to read an array's specified index.
 
 	Arguments:
-		Array: The List that we want to check a specific index.
-		Index: The Index element we want to read.
+		Array (list[object]*): The List that we want to check a specific index.
+		Index (int*): The Index element we want to read.
+
 	Returns:
-		The returned item can be anything. In the case of a failed read, the return value will always be none.
+		object/None: The returned item can be anything. In the case of a failed read, the return value will always be None.
+
+	Examples:
+		>>> Array: list[str] = ["Arellayn was here"];
+		>>> Safe.Index(Array, 0);
+		"Arellayn was here"
+		>>> Safe.Index(Array, 1);
+		None
 	"""
 	if (Index > len(Array)): return None;
 	return Array[Index];
