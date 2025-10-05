e3d58427bb0d17f7cddbebaa4d6c2cc067939ce6
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2025-10-05 02:01:02 +0200

    Config | DocString Overhaul

--- TSN_Abstracter/Config.py
+++ TSN_Abstracter/Config.py
@@ -1,13 +1,33 @@
+"""
+This module from TSN Abstracter is in charge of configuring its behavior.  
+
+### Available Configuration Classes:
+**Logger**: Class used to configure settings related to the TSNA Logger.  
+"""
+
 class Logger:
 	"""
 	Class used to configure settings related to the TSNA Logger.  
 
-	- File
-		→ Specifies whenever we allow writing the Log to a File.  
-	- File_Level
-		→ The minimum Log level that we should log to a file.  
-	- Print_Level
-		→ The minimum Log level that we should log to the console.  
+	### General Settings:
+	**Disable** : *bool = False*  
+	Global toggle to entirely disable the TSNA Loggers.  
+	**Display_Date** : *bool = True*  
+	Toggle displaying the date at the beginning of Log Entries.  
+	**Display_Caller** : *bool = True*  
+	Toggle displaying function that called TSNA to Log a brand new Entry.  
+
+	### File Logger:
+	**File** : *bool = False*  
+	Specifies whenever we allow writing the Log to a File.  
+	**File_Level** : *int = 20*  
+	The minimum Log level that we should log to a file.  
+	**File_Folder** : *str = "Logs"*  
+	The relative (or absolute) folder name that will be used to save TSNA Logs.  
+
+	### Text Logger
+	**Print_Level** : *int = 20*  
+	The minimum Log level that we should log to the console.  
 	"""
 	Disable: bool = False;
 	Display_Date: bool = True;
