8f657370939fd073886eb3359ccfab282ce2c7ad
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2025-10-06 16:06:26 +0200

    Cryptography | Strict Type Hinting & Type Hinting Fixes

--- TSN_Abstracter/Cryptography.py
+++ TSN_Abstracter/Cryptography.py
@@ -1,24 +1,23 @@
 """
 This module from TSN Abstracter contains various random functions handle encryption/cryptography.
 
-# THIS MODULE IS CURRENTLY DEPRECATED, DO NOT USE IT FOR NOW.
+# THIS MODULE IS CURRENTLY UNSUPPORTED/DEPRECATED, DO NOT USE IT FOR NOW.
 """
-
 import TSN_Abstracter.Log as Log;
 import hashlib;
 
-import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519;
-import cryptography.hazmat.primitives.asymmetric.rsa as rsa;
+from cryptography.hazmat.primitives.asymmetric import ed25519, rsa, padding, types;
+from cryptography.hazmat.primitives import serialization, hashes;
 
-import cryptography.hazmat.primitives.asymmetric.padding as padding;
-import cryptography.hazmat.primitives.serialization as serialization;
-import cryptography.hazmat.primitives.hashes as hashes;
 
-def Generate_Key(Key_Size: int = 4096) -> rsa.RSAPrivateKey | rsa.RSAPublicKey:
+
+
+def Generate_Key(Key_Size: int = 4096) -> tuple[rsa.RSAPrivateKey, rsa.RSAPublicKey]:
 	""" Generates a pair of RSA Private/Public Keys with a size of 4096 bits by default. """
 	
 	Private_Key = Generate_Private(Key_Size);
 	return Private_Key, Generate_Public(Private_Key);
+
 
 def Generate_Private(Key_Size: int = 4096) -> rsa.RSAPrivateKey:
 	""" Generates a pair of RSA Private Key with a size of 4096 bits by default. """
@@ -28,10 +27,15 @@ def Generate_Private(Key_Size: int = 4096) -> rsa.RSAPrivateKey:
 		key_size = Key_Size
 	);
 
+
 def Generate_Public(Private_Key: rsa.RSAPrivateKey) -> rsa.RSAPublicKey:
 	""" Generates a RSA Public Key from its Private Key. """
 	Log.TSN_Debug(f"Generating RSA Public Key...");
 	return Private_Key.public_key();
+
+
+
+
 
 def Get_Bytes_Public(Key: rsa.RSAPublicKey) -> bytes:
 	""" Takes in an RSA public key and returns its bytes in PEM Encoding. """
@@ -40,7 +44,8 @@ def Get_Bytes_Public(Key: rsa.RSAPublicKey) -> bytes:
 		format=serialization.PublicFormat.SubjectPublicKeyInfo
 	);
 
-def Get_Bytes_Private(Key: rsa.RSAPublicKey) -> bytes:
+
+def Get_Bytes_Private(Key: rsa.RSAPrivateKey) -> bytes:
 	""" Takes in an RSA private key and returns its bytes in PEM Encoding (UNPROTECTED). """
 	return Key.private_bytes(
 		encoding=serialization.Encoding.PEM,
@@ -48,13 +53,22 @@ def Get_Bytes_Private(Key: rsa.RSAPublicKey) -> bytes:
 		encryption_algorithm=serialization.NoEncryption()
 	);
 
-def Load_Public(Key: bytes) -> rsa.RSAPublicKey:
+
+
+
+
+def Load_Public(Key: bytes) -> types.PublicKeyTypes:
 	""" Takes in PEM Bytes and attempts to return it as a Public Key RSA Object. """
 	return serialization.load_pem_public_key(Key);
 
-def Load_Private(Key: bytes) -> rsa.RSAPrivateKey:
+
+def Load_Private(Key: bytes) -> types.PrivateKeyTypes:
 	""" Takes in PEM Bytes and attempts to return it as a Public Key RSA Object. """
-	return serialization.load_pem_private_key(Key)
+	return serialization.load_pem_private_key(Key, None);
+
+
+
+
 
 def Encrypt(Public_Key: rsa.RSAPublicKey, Data: str) -> bytes:
 	""" Encrypts Data using RSA-4096 and returns the encrypted Bytes. """
@@ -67,6 +81,7 @@ def Encrypt(Public_Key: rsa.RSAPublicKey, Data: str) -> bytes:
 		)
 	);
 
+
 def Decrypt(Private_Key: rsa.RSAPrivateKey, Data: bytes) -> str:
 	""" Decrypts Data using RSA-4096 and returns as a String. """
 	return Private_Key.decrypt(
@@ -78,13 +93,17 @@ def Decrypt(Private_Key: rsa.RSAPrivateKey, Data: bytes) -> str:
 		)
 	).decode("utf-8");
 
+
 def Hash(Data: str) -> str:
 	""" Returns the SHA3_512 Hex Hash of Data """
 	return (hashlib.sha3_512(Data.encode("utf-8"))).hexdigest();
 
 
+
+
+
 # To not be used, I couldn't read and coded these in not realizing I wanted RSA instead.
-def Generate_Signature() -> ed25519.Ed25519PrivateKey | ed25519.Ed25519PublicKey:
+def Generate_Signature() -> tuple[ed25519.Ed25519PrivateKey, ed25519.Ed25519PublicKey]:
 	""" Untested, do not use; Generates ed25519 Private Key / Public Key Combo"""
 	Private_Key = ed25519.Ed25519PrivateKey.generate();
 	Public_Key = Private_Key.public_key();
